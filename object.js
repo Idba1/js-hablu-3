5800d9c09ca12df0c3e40cc790ee997dc207fa7b
Idba1 <moniraislamself@gmail.com>
2024-02-24 22:59:33 +0600

    object

--- object.js
+++ object.js
@@ -6,5 +6,22 @@ function myInfo(yourName, village, phoneNo){
     this.phoneNo = phoneNo;
 }
 
-let newObj = new myInfo("Idba", "Dhaka", +8801222222);
-console.log(newObj);
+let newObj1 = new myInfo("Idba", "Dhaka", +8801222222);
+console.log(newObj1);
+
+
+function myInfo(yourName, village, phoneNo){
+    this.yourName = yourName;
+    this.village = village;
+    this.phoneNo = phoneNo;
+    this.showMe = function(){
+        console.log(this.yourName);
+        console.log(this.village);
+        console.log(this.phoneNo);
+    }
+}
+
+/* let newObj2 = new myInfo("Idba", "Dhaka", +8801222222);
+console.log(newObj2); */
+let student =new myInfo ("Rifa", "hossennagor", 1001222);
+student.showMe();
