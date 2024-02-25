7568485229890028eabcd4a55b03d2445b3c5790
Idba1 <moniraislamself@gmail.com>
2024-02-26 03:35:54 +0600

    inner function

---
+++ inner-function.js
@@ -0,0 +1,10 @@
+function world(){
+    let myName = "Idba";
+    // console.log(lastName);
+    function country(){
+        let lastName = "Islam";
+        console.log(myName, lastName);
+    }
+    country()
+}
+world()
